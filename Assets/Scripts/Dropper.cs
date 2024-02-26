826afca2090e3dce4d7eb200a66446b0d511e90f
Jade1724 <haruka.i1724@gmail.com>
2024-02-26 20:30:34 +1300

    Dropper visible and falling after 3 seconds

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
+    MeshRenderer renderer;
+    Rigidbody rigidbody;
     [SerializeField] float timeToWait = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        renderer = GetComponent<MeshRenderer>();
+        rigidbody = GetComponent<Rigidbody>();
+        renderer.enabled = false;
+        rigidbody.useGravity = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Time.time > timeToWait) {
-            Debug.Log(Time.time);
+            renderer.enabled = true;
+            rigidbody.useGravity = true;
         }
     }
 }
